e28dcae4a055fc4369e5c2c1d3da2541b413b578
Laure <laure.milian@gmail.com>
2016-11-02 13:51:43 +0100

    fix momentjs

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 		},
 		
 		listeners : function() {
-			$("#checkButton").on("click", this.checkInputs);
+			$("#checkButton").on("click", this.checkInputs.bind(this));
 		},
 
 		checkInputs : function() {
@@ -22,24 +22,21 @@
 			
 			// vérifier si les inputs correspondent aux attentes, si oui, lancer la fonction getDate
 			if ($inputDay < 1 || $inputDay > 31 || !$inputDay) {
-				app.inputError("divInputDay", "La date du jour doit être comprise entre 1 et 31");
+				this.inputError("divInputDay", "La date du jour doit être comprise entre 1 et 31");
 			} 
 			else if ($inputYear < 0 || !$inputYear) {
-				app.inputError("divInputYear", "L'année doit être supérieure à 0");
+				this.inputError("divInputYear", "L'année doit être supérieure à 0");
 			} 
 			else {
-				app.getDate($inputDay, $selectedMonth, $inputYear);
+				this.getDate($inputDay, $selectedMonth, $inputYear);
 			}
 
 		},
 
 		getDate : function(day, month, year) {
 			var date = moment(year + "-" + month + "-" + day);
-			var correspondingDay = date.day();
-			console.log(correspondingDay);
-			var daysArr = ["dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"];
-			var dayToDisplay = daysArr[correspondingDay];
-			app.displayDay(dayToDisplay);
+			var dayToDisplay = date.lang('fr').format('dddd');
+			this.displayDay(dayToDisplay);
 		},
 
 		displayDay : function(day) {
